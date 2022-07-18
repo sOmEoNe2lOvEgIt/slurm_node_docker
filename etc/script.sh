dd258a7eaf6ffe2a6d699f800919f169fd760787
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-07-18 10:43:02 +0200

    Updated run and inside script as wemm as conf files.

--- etc/script.sh
+++ etc/script.sh
@@ -2,6 +2,4 @@
 chown 999 /etc/munge/munge.key
 mkdir /var/spool/slurmd
 su -s /bin/bash -c "munged --key-file /etc/munge/munge.key" munge
-# su -c "chmod +rw -R /sys/fs/cgroup"
 /usr/sbin/slurmd -D -s
-/bin/bash
