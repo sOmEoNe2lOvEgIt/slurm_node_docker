c19ec46bcaf9026e4a33986925ec08bc31ddee2d
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-07-26 16:17:49 +0200

    Updated script

--- etc/script.sh
+++ etc/script.sh
@@ -5,4 +5,5 @@ mkdir /node_logs
 chmod 700 /node_logs
 useradd ray -u 42
 su -s /bin/bash -c "munged --key-file /etc/munge/munge.key" munge
-su -c "/usr/sbin/slurmd -Dvvv -s"
+#su -c "/usr/sbin/slurmd -Dvvv -s"
+su -c "/bin/bash"
