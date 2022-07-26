d4e53851ec15a9fa38a844386f0c2cc78fc52aa6
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-07-26 16:34:24 +0200

    Added back slurm_d as start daemon, mounted fake tmpfs for cgroup.

--- etc/script.sh
+++ etc/script.sh
@@ -4,6 +4,7 @@ chown 999 /etc/munge/munge.key
 mkdir /node_logs
 chmod 700 /node_logs
 useradd ray -u 42
+mkdir cgroup
+mount -t tmpfs /cgroup
 su -s /bin/bash -c "munged --key-file /etc/munge/munge.key" munge
-#su -c "/usr/sbin/slurmd -Dvvv -s"
-su -c "/bin/bash"
+su -c "/usr/sbin/slurmd -Dvvv -s"
