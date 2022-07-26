e52aa36eb2bc89c9794f4a49b1b9e89ed6636b8d
sOmEoNe2lOvE <raymcraft@gmail.com>
2022-07-26 13:08:54 +0200

    Added new dir for nodes to store jobs logs.

--- etc/script.sh
+++ etc/script.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 chmod 700 etc/munge/munge.key
 chown 999 /etc/munge/munge.key
+mkdir /node_logs
+chmod 700 /node_logs
 su -s /bin/bash -c "munged --key-file /etc/munge/munge.key" munge
-/usr/sbin/slurmd -Dvvv -s 
+su -c "/usr/sbin/slurmd -Dvvv -s"
